a3fa870bcf155998fd3bb3a983b39cf32b4bbe91
rea-sna <reoshigeno0708@gmail.com>
2021-04-06 18:45:25 +0900

    Initial Commit

---
+++ SkipDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkipDemo
+//
+//  Created by 繁野怜央 on 2021/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
